4e6e537bfec0ab3dada43420a2bf44933744eda7
JacobTheLiar <jacob_the_liar@tlen.pl>
2020-01-17 11:35:13 +0100

    initial commit

---
+++ src/test/kotlin/pl/jacobtheliar/sandbox/kotlin/SpringKotlinTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package pl.jacobtheliar.sandbox.kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringKotlinTestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
